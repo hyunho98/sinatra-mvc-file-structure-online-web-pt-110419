fa4cd7942c24d63b459da6bb1b63a6f998d82796
Alex Lee <hyunho98@gmail.com>
2020-02-26 02:50:11 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,9 +14,9 @@ class ApplicationController < Sinatra::Base
     attr_accessor :name, :breed, :age
 
     def initialize(attributes)
-      @name = name
-      @breed = breed
-      @age = age
+      @name = :name
+      @breed = :breed
+      @age = :age
     end
   end
 end
